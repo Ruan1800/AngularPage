564509c8d717e8869aec22f16e637788956fe569
Ruan Ribeiro <Rrjalfim@gmail.com>
2024-06-09 20:50:02 -0300

    resolving last components

--- landingg-page/src/app/app.routes.ts
+++ landingg-page/src/app/app.routes.ts
@@ -1,3 +1,11 @@
+import { Component } from '@angular/core';
+import { Comment } from './../../node_modules/@types/estree/index.d';
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  }
+];
